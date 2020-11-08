190323fb43042cbbafa7f3059ae5fc5ceb24984b
Hagai M <hagaim@mail.tau.ac.il>
2020-11-08 13:58:38 +0200

    added nick removal capabilities

--- main.py
+++ main.py
@@ -68,6 +68,11 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity=act)
 
 @bot.event
+async def on_member_update(before, after):
+    if before.id == bot.user.id:
+        await after.edit(nick=None)
+
+@bot.event
 async def on_disconnect():
     print("Oops! Disconnected...")
 
